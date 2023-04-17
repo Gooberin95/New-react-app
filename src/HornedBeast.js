8c79d757bad5b1d76842fb0549601704a2fdedda
Brandon Ruiz <Sober4202020@outlook.com>
2023-04-17 00:51:56 +0000

    adding in the arguments for compenents

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,3 +1,4 @@
+import { toHaveDisplayValue } from "@testing-library/jest-dom/dist/matchers";
 import React from "react"; 
 
 class HornedBeast extends React.Component {
@@ -5,8 +6,8 @@ class HornedBeast extends React.Component {
         return(
             <>
                 <h2>GoatandHorns</h2>
-                <img title='hello world' src='https://th.bing.com/th/id/R.8ca785462ba14ede2869fd2ce28dd169?rik=TIy8OF32LjEWDA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-XdRtST5NkrM%2fT7z433dfS9I%2fAAAAAAAAGFs%2fA7Agez6HAxs%2fs1600%2fmountain_goat.jpg&ehk=Jmzy64yYh9hlUA21rcqAjiZFMG%2ffBQuLmiUvmUS6ZS0%3d&risl=&pid=ImgRaw&r=0' alt='HornedAnimalHere'/>
-                <p>This goat is native the the desert islands of florida where temperatures reach below 3 degrees celcius</p>            
+                <img> {this.props.title}</img>
+                <p>{this.props.description}</p>            
             </>
         )
     }
--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,7 @@ class Main extends React.Component {
     render() {
         return(
             <>
-            <HornedBeast/>
+            <HornedBeast title={'Hello World'} description={'Horned animals goat thing that walks among us, beware and bekind'} imageUrl={'https://th.bing.com/th/id/R.8ca785462ba14ede2869fd2ce28dd169?rik=TIy8OF32LjEWDA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-XdRtST5NkrM%2fT7z433dfS9I%2fAAAAAAAAGFs%2fA7Agez6HAxs%2fs1600%2fmountain_goat.jpg&ehk=Jmzy64yYh9hlUA21rcqAjiZFMG%2ffBQuLmiUvmUS6ZS0%3d&risl=&pid=ImgRaw&r=0'}/>
 
             
             </>
