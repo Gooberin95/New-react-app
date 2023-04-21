27f13862166f328270312e282c2c924c23b1e424
Brandon Ruiz <Sober4202020@outlook.com>
2023-04-21 05:18:43 +0000

    got the number to show for favoriting

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,14 +1,24 @@
-import data from "./data.json";
+
 import React from "react"; 
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favnum: 0,
+            minusfav: 0
+    }
+}
     render() {
-
         return(
             <>
+            <div>
+                
                 <h2>{this.props.title}</h2>
                 <img src={this.props.imageUrl}/>
-                <p>{this.props.description}</p>            
+                <p>{this.props.description}</p>
+                <p>{this.state.favnum}</p>  
+            </div>       
             </>
         )
     }
